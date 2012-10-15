81e4b6a709660a49c2db13f28a81260be66b2824
Allison Hall <allison.d.hall@gmail.com>
2012-10-14 19:16:24 -0700

    original files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YScholar::Application.config.secret_token = '760026928071d2f25c933e2744ce6b59c182e8334f0817598ee655673a1b39de4e5ce930569908c93e5e347f698a28335b52682e733f9c434242a97db4ccfddf'
