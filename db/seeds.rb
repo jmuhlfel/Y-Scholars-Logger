4fe60bafe2f2df5791e23bb5d7fadc652a3ba878
Robin Sylvan <rsylvan@berkeley.edu>
2012-10-27 17:04:59 -0700

    Added db:seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!({:child_email => '',
+      :email => "admin@y-scholars",
+      :name => 'Administrator',
+      :password => 'abc123',
+      :password_confirmation => 'abc123',
+      :type => 'Administrator'})
