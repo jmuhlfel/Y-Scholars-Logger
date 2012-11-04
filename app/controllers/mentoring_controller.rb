e0c5e761e1f76b596be0666466d41b0cfe8c0974
Josh Muhlfelder <jmuhlfel@berkeley.edu>
2012-11-04 03:07:41 -0800

    added auth checks for signing students in/out

--- app/controllers/mentoring_controller.rb
+++ app/controllers/mentoring_controller.rb
@@ -2,14 +2,16 @@ class MentoringController < ApplicationController
 
   def signin
     student = Student.find_by_email(params[:student_email])
-    raise unless not student.is_signed_in?
+    raise "Student already signed in" unless not student.is_signed_in?
+    raise "You don't have permission to sign students in" unless current_user.is_tutor? or current_user.is_admin?
     #add student sign in logic here
     redirect_to :dashboard
   end
   
   def signout
     student = Student.find_by_email(params[:student_email])
-    raise unless student.is_signed_in?
+    raise "Student not signed in" unless student.is_signed_in?
+    raise "You don't have permission to sign students out" unless current_user.is_tutor? or current_user.is_admin?
     #add student sign out logic here
     redirect_to :dashboard
   end
