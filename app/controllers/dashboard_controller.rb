26a50713248266f3283604f815be807a005b40ec
Robin Sylvan <rsylvan@berkeley.edu>
2012-11-18 16:40:16 -0800

    Slight changes to the student dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -52,6 +52,13 @@ class DashboardController < ApplicationController
     @total_hours = total_seconds / 3600
     @start_date = start_sun.to_date
     @end_date = @start_date + 7.day
+    red = "#f00"
+    green = "#00FF00"
+    if @total_hours / @required_hours.to_i >= 1
+      @color = green
+    else
+      @color = red
+    end
     render :student_dashboard
   end
   
